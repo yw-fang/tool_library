24120058bf3fe91b380b41c4df8e34bcece543fa
yw-fang <fyuewen@gmail.com>
2017-12-15 15:03:40 +0900

    revise

--- linux-shell/loop-enth.sh
+++ linux-shell/loop-enth.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ##for((i=20;i<=120;i=i+20));
-#for((i=200;i<=800;i=i+100));
 for i in  0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
 do
 #          echo -n $i'     ' >> ./toten.dat; greptoten-fang.sh $i/OUTCAR | gawk '{print $5}' >> ./toten.dat
