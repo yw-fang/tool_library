5722e5c6167e20a1c50c1ad2b2866f6ff1d5831b
yw-fang <fyuewen@gmail.com>
2018-01-18 12:34:53 +0900

    print the execution time

--- python-tool/deletdir/deletdir.py
+++ python-tool/deletdir/deletdir.py
@@ -3,8 +3,11 @@
 #Date: 18th Jan. 2018 creted
 # I found linux shell is quite slow in some cluster when deleting directory cotnaining thouansands of files, that's why use this alternative way by python
 import shutil as st
+import time
+start_time=time.time()
 print('Please make sure you '
       'are in the folder which contains the directory you '
       'want to delete')
 dele_dir = input('please input the directory you wnat to delete: ')
 st.rmtree(dele_dir)
+print("--- %s seconds ---" % (time.time() - start_time))
