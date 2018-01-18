4d8e21361038ec36b0a7223cb19f20957586d2fb
yw-fang <fyuewen@gmail.com>
2018-01-18 16:55:32 +0900

    tar file with excluding some specfied patterns

---
+++ linuxshell-tool/tarlzma-excludefile.sh
@@ -0,0 +1,4 @@
+#tar files with excluding some patters
+#here I tar a directory to a compressed file with exlcuing the files with nameof wav*, WAVEVAR...
+env GZIP=-9 tar -caf $1.tar.lamz $1 --exclude=wav* --exclude=WACECAR --exclude=tot-DOS* --exclude=*.o* .
+
