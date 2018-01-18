135ab40f25730230c6aebaa9c2d352ed55fd4721
yw-fang <fyuewen@gmail.com>
2018-01-18 11:58:58 +0900

    add author, date, purpose for deletdir.py

--- python-tool/deletdir/deletdir.py
+++ python-tool/deletdir/deletdir.py
@@ -1,3 +1,7 @@
+#DELET DIRECTORY is the purpose of this script
+#Author: Y.-W. FANG
+#Date: 18th Jan. 2018 creted
+# I found linux shell is quite slow in some cluster when deleting directory cotnaining thouansands of files, that's why use this alternative way by python
 import shutil as st
 print('Please make sure you '
       'are in the folder which contains the directory you '
