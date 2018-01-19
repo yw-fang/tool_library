f15a1c0cc25559f7516f7081aa40ea40e8ee8286
yw-fang <fyuewen@gmail.com>
2018-01-19 22:35:41 +0900

    comment the invalid command, add new commad to tar excluding specified files

--- linuxshell-tool/tarlzma-excludefile.sh
+++ linuxshell-tool/tarlzma-excludefile.sh
@@ -1,5 +1,9 @@
 #author: Y.-W. FANG
 #tar files with excluding some patters
 #here I tar a directory to a compressed file with exlcuing the files with nameof wav*, WAVEVAR...
-env GZIP=-9 tar -caf $1.tar.lamz $1 --exclude=wav* --exclude=WACECAR --exclude=tot-DOS* --exclude=*.o* .
+
+#Method-1: this doesn't work in my ubuntu 17 and mac os
+#env GZIP=-9 tar -caf $1.tar.lamz $1 --exclude=wav* --exclude=WACECAR --exclude=tot-DOS* --exclude=*.o* .
+#Method-2
+env GZIP=-9 tar --exclude */WAVECAR --exclude */*/WAVECAR --exclude */*/*/WAVECAR --exclude */*/*/*/WAVECAR -caf c2.tar.lzma c2
 
