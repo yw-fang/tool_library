62b17507919b8e7416beb91b13050a17f8437f15
yw-fang <fyuewen@gmail.com>
2018-02-19 14:58:58 +0900

    git delete remote/lcoal branchs

---
+++ linuxshell-tool/git/git_d_branch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#delete local branch
+#Yue-Wen FANG, Feb. 2018
+git branch -d $1 #ref: https://www.git-tower.com/learn/git/faq/delete-remote-branch
---
+++ linuxshell-tool/git/git_d_branch_remote.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#delete remote branch
+#Yue-Wen FANG, Feb. 2018
+git push origin --delete $1 $ref: https://www.git-tower.com/learn/git/faq/delete-remote-branch
