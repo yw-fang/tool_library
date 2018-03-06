f18b69cb95dc12dd523183e9a8ba927c2eb61cc0
yw-fang <fyuewen@gmail.com>
2018-03-06 21:30:46 +0900

    add a refrence, author info

--- linuxshell-tool/runjupyter_server_for_remote.sh
+++ linuxshell-tool/runjupyter_server_for_remote.sh
@@ -1,2 +1,6 @@
+#This script should be used with remotejupyter-asura-lab or others to get access remote jupyter notebooks
+#Author: Yue-Wen FANG
+#Date: March 6th, 2018
 nohup /home/ywfang/miniconda3/envs/aiida/bin/python /home/ywfang/miniconda3/bin/jupyter-notebook --no-browser --port=8889 &
 
+#ref:http://amber-md.github.io/pytraj/latest/tutorials/remote_jupyter_notebook
