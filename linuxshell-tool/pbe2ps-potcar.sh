336451717dd5d371e455687a800ee08478840a0b
yw-fang <fyuewen@gmail.com>
2018-03-29 20:28:16 +0900

    changed the default POTCAR to any filename

--- linuxshell-tool/pbe2ps-potcar.sh
+++ linuxshell-tool/pbe2ps-potcar.sh
@@ -1,5 +1,5 @@
 #Author: Yue-Wen FANG
 #Purpose: Change PBE to PS for POTCAR used in VASP
-sed -i 's/LEXCH  = PE/LEXCH  = PS/' POTCAR
+sed -i 's/LEXCH  = PE/LEXCH  = PS/' $1
 echo 'LEXCH in POTCAR chaged to PS'
-head 10 POTCAR
+head 10 $1
