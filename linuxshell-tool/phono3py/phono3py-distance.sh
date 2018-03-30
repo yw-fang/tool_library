ba6fe7c4061a21edae15ff6094215ab214bb2e9d
yw-fang <fyuewen@gmail.com>
2018-03-30 23:23:33 +0900

    sort -V, fix bug

--- linuxshell-tool/phono3py/phono3py-distance.sh
+++ linuxshell-tool/phono3py/phono3py-distance.sh
@@ -1,4 +1,5 @@
 #This script read the disp.yaml or disp_fc3.yaml or disp_fc2.yaml
 #and print the distances
 #Yue-Wen FANG, 2018 March 19th
-egrep '^\s+distance' $1|awk '{print $2}'|sort|uniq
+#revision data: 2018 March 30th, add '-V' to fix some sort error
+egrep '^\s+distance' $1|awk '{print $2}'|sort -V|uniq
