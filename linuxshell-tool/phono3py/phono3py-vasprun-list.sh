cc2d29ef88d03a3abd955a5054c7446d448ac481
yw-fang <fyuewen@gmail.com>
2018-03-30 14:52:16 +0900

    phono3py list vasprun.xml

---
+++ linuxshell-tool/phono3py/phono3py-vasprun-list.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for i in `ls POSCAR-0*|sed s/POSCAR-//`;do echo disp-$i/vasprun.xml;done > file_list.dat
+	echo 'the vasprun.xml list isin file_list.dat'
+	echo "plese use it through 'phono3py --cf3_file file_list.dat' "
