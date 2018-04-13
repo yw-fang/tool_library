c2271c5592d220491eee113d2a658a32ffd24d98
yw-fang <fyuewen@gmail.com>
2018-04-13 22:24:33 +0900

    format python script

--- python-tool/deletdir/deletdir.py
+++ python-tool/deletdir/deletdir.py
@@ -1,10 +1,11 @@
-#DELET DIRECTORY is the purpose of this script
-#Author: Y.-W. FANG
-#Date: 18th Jan. 2018 created
-# I found linux shell is quite slow in some cluster when deleting directory cotnaining thouansands of files, that's why use this alternative way by python
+# DELET DIRECTORY is the purpose of this script
+# Author: Y.-W. FANG
+# Date: 18th Jan. 2018 created
+# Linux shell is quite slow in some clusters when deleting directory
+# containing thouansands of files, that's why I use python.
 import shutil as st
 import time
-start_time=time.time()
+start_time = time.time()
 print('Please make sure you '
       'are in the folder which contains the directory you '
       'want to delete')
