590755d4aacb1fbfa6f4855c34018a84c209f3a4
yw-fang <fyuewen@gmail.com>
2018-05-07 11:51:21 +0900

    calculate volume for vasp structure'

---
+++ python-tool/vasp-volume.py
@@ -0,0 +1,50 @@
+import pymatgen as mg
+import os
+import re
+
+__author__ = "Yue-Wen FANG"
+__maintainer__ = "Yue-Wen FANG"
+__email__ = "fyuewen@gmail.com"
+__status__ = "Development"
+__creation_date__ = "May 7, 2018"
+
+
+path = "./"
+
+# Read every file with extension of '.vasp' in directory
+filelist = []
+for filename in os.listdir(path):
+    name, file_extension = os.path.splitext(filename)
+    boolean_value = file_extension == '.vasp'
+    if boolean_value:
+        filelist.append(name)
+# print(filelist)
+
+
+# #################define functions##################
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    or
+    https://stackoverflow.com/questions/5967500/
+    how-to-correctly-sort-a-string-with-a-number-inside?
+    utm_medium=organic&utm_source=google_rich_qa&utm_campai
+    gn=google_rich_qa
+    '''
+    return [atoi(c) for c in re.split('(\d+)', text)]
+# ####################################################
+
+
+filelist.sort(key=natural_keys)
+# print(filelist)
+
+for file in filelist:
+    file_vasp = file + '.vasp'
+    structure = mg.Structure.from_file(file_vasp)
+    print(file, "vol = {}".format(structure.volume/2))
