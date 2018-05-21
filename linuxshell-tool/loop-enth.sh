bfae5b59470971b8958165b73c2d833316046e1e
yw-fang <fyuewen@gmail.com>
2018-05-21 22:56:25 +0900

    remove and merge it with loop-enthalpy.sh

--- linuxshell-tool/loop-enth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# display enthalpy from OUTCAR
-##for((i=20;i<=120;i=i+20));
-for i in  0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
-do
-#          echo -n $i'     ' >> ./toten.dat; greptoten-fang.sh $i/OUTCAR | gawk '{print $5}' >> ./toten.dat
- echo -n $i'     ' >> ./en.dat; grep enth $i/OUTCAR |  gawk '{print $5}' >> ./en.dat
-done
