c447d3e5cb0df555cfd4ded81716a59e272be0b2
yw-fang <fyuewen@gmail.com>
2018-06-06 00:03:06 +0900

    comment out, I will think about it again

--- linuxshell-tool/phono3py/phono3py-printltc_for_each_cutoff.sh
+++ linuxshell-tool/phono3py/phono3py-printltc_for_each_cutoff.sh
@@ -1,5 +1,5 @@
-a=$(ls -l structure_list* | wc -l)
-echo $a
-grep 300.0 {1..$a}/_scheduler-stdout.txt | gawk '{print $3" "$4" "$5}' > tmp1.dat
+##a=$(ls -l structure_list* | wc -l)
+##echo $a
+grep 300.0 {1..39}/_scheduler-stdout.txt | gawk '{print $3" "$4" "$5}' > tmp1.dat
 paste -d '  ' list/tmp.dat tmp1.dat > cutoff-ltc.csv
 
