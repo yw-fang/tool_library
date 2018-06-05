9d99d215e9238e3408698c70e1172df8c0f28184
yw-fang <fyuewen@gmail.com>
2018-06-06 00:34:42 +0900

    print all LTC at each cutoff

--- linuxshell-tool/phono3py/phono3py-printltc_for_each_cutoff.sh
+++ linuxshell-tool/phono3py/phono3py-printltc_for_each_cutoff.sh
@@ -1,5 +1,16 @@
-##a=$(ls -l structure_list* | wc -l)
-##echo $a
-grep 300.0 {1..39}/_scheduler-stdout.txt | gawk '{print $3" "$4" "$5}' > tmp1.dat
+## This script prints the LTC at 300 K.
+## Author: Y.-W.FANG at Kyoto University
+number_of_cutoff=$(ls -l structure_list* | wc -l)
+echo $numer_of_cutoff
+line=$(gawk '/xx/{print NR}' 1/_scheduler-stdout.txt)  #locate the line with keyword 'xx'
+echo $line
+roomline=$(expr $line+31 | bc)  #locate the line with LTC at 300 K
+echo $roomline
+rm -rf tmp1.dat
+rm -rf  cutoff-ltc.csv
+for ((i=1; i<=$number_of_cutoff; i++))
+do
+sed -n "$roomline p" $i/_scheduler-stdout.txt | gawk '{print $2" "$3" "$4}' >> tmp1.dat
+done
+#grep 300.0 {1..39}/_scheduler-stdout.txt | gawk '{print $3" "$4" "$5}' > tmp1.dat
 paste -d '  ' list/tmp.dat tmp1.dat > cutoff-ltc.csv
-
