f259651584e2d59a13d834a1e622bd21eef09e62
yw-fang <fyuewen@gmail.com>
2018-06-05 23:48:24 +0900

    print LTC at 300K by counting the number of structure_list

--- linuxshell-tool/phono3py/phono3py-printltc_for_each_cutoff.sh
+++ linuxshell-tool/phono3py/phono3py-printltc_for_each_cutoff.sh
@@ -1,3 +1,5 @@
-grep 300.0 {1..10}/_scheduler-stdout.txt | gawk '{print $3" "$4" "$5}' > tmp1.dat
+a=$(ls -l structure_list* | wc -l)
+echo $a
+grep 300.0 {1..$a}/_scheduler-stdout.txt | gawk '{print $3" "$4" "$5}' > tmp1.dat
 paste -d '  ' list/tmp.dat tmp1.dat > cutoff-ltc.csv
 
