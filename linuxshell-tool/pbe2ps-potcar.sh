1a2f551651a665b02d34a75d1206c97483083442
yw-fang <fyuewen@gmail.com>
2018-07-10 02:35:29 +0900

    change PBE to PS in POTCAR, unfinished

--- linuxshell-tool/pbe2ps-potcar.sh
+++ linuxshell-tool/pbe2ps-potcar.sh
@@ -1,5 +1,8 @@
 #Author: Yue-Wen FANG
 #Purpose: Change PBE to PS for POTCAR used in VASP
-sed -i 's/LEXCH  = PE/LEXCH  = PS/' $1
+#sed  "s/LEXCH  = PS/LEXCH  = PS/g" << $1
+echo 'usage: run pbe2ps-potcar.sh directly'
+echo 'make sure you have a POTCAR in current dir'
+sed -i '' 's/LEXCH  = PS/LEXCH  = PS/g' POTCAR
 echo 'LEXCH in POTCAR chaged to PS'
-head 10 $1
+head -10 POTCAR
