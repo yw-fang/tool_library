274ae80fa60ad8e5d10f6041d756e40cfe2827f6
yw-fang <fyuewen@gmail.com>
2018-07-10 02:36:21 +0900

    pbe2pbs not work for time being

--- linuxshell-tool/pbe2ps-potcar.sh
+++ linuxshell-tool/pbe2ps-potcar.sh
@@ -1,5 +1,6 @@
 #Author: Yue-Wen FANG
 #Purpose: Change PBE to PS for POTCAR used in VASP
+#Status: This cannot work for time being
 #sed  "s/LEXCH  = PS/LEXCH  = PS/g" << $1
 echo 'usage: run pbe2ps-potcar.sh directly'
 echo 'make sure you have a POTCAR in current dir'
