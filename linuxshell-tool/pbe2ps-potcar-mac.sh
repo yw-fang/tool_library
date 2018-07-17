d20c1a4336eb38a68cb534ec54f6fcbe45423a51
yw-fang <fyuewen@gmail.com>
2018-07-18 00:01:43 +0900

    fix an error

--- linuxshell-tool/pbe2ps-potcar-mac.sh
+++ linuxshell-tool/pbe2ps-potcar-mac.sh
@@ -4,6 +4,6 @@
 #sed  "s/LEXCH  = PS/LEXCH  = PS/g" << $1
 echo 'usage: run pbe2ps-potcar.sh directly'
 echo 'make sure you have a POTCAR in current dir'
-sed -i '' 's/LEXCH  = PS/LEXCH  = PS/g' POTCAR
+sed -i '' 's/LEXCH  = PE/LEXCH  = PS/g' POTCAR
 echo 'LEXCH in POTCAR chaged to PS'
 head -10 POTCAR
