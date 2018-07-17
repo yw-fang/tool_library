b4462478c2b34dfca38de306c09e047f91e6597a
yw-fang <fyuewen@gmail.com>
2018-07-17 23:51:55 +0900

    add seq method for 'for loo'

--- linuxshell-tool/delete.sh
+++ linuxshell-tool/delete.sh
@@ -1,4 +1,8 @@
+# This script can be used to delete directories
+# which are indexed by enumarated numbers.
 for(( i=1; i<=50; i++ ));
+	# alternatively, we can also use
+	# for i in $(seq 0 10 90)
 do
   rm -rf $i/
 done
