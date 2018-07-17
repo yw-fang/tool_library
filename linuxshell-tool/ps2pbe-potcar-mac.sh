01f1be3851c66d5e6a02b2880fde0191cd7593c5
yw-fang <fyuewen@gmail.com>
2018-07-18 00:00:19 +0900

    update script for ps2pbe

--- linuxshell-tool/ps2pbe-potcar-mac.sh
+++ linuxshell-tool/ps2pbe-potcar-mac.sh
@@ -1,5 +1,6 @@
 #Author: Yue-Wen FANG
 #Purpose: Change PS to PBE for POTCAR used in VASP
-sed -i 's/LEXCH  = PS/LEXCH  = PE/' $1
+sed -i '' 's/LEXCH  = PS/LEXCH  = PE/g' POTCAR
+# sed -i 's/LEXCH  = PS/LEXCH  = PE/' $1
 echo 'LEXCH in POTCAR chaged to PE'
-head 10 $1
+head 10 POTCAR
