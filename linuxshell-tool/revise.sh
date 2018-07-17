62b53b07a03c6dd2017448f7530ff672f1610276
yw-fang <fyuewen@gmail.com>
2018-07-17 23:45:19 +0900

    remove an inactive script

--- linuxshell-tool/revise.sh
+++
@@ -1,8 +0,0 @@
-#cp job-osepv52.pbs I/
-cd I; cp POSCAR INCAR KPOINTS job-osepv52.pbs ../../$1/I; cd ../
-#cp job-osepv52.pbs II/
-cd II; cp POSCAR INCAR KPOINTS job-osepv52.pbs ../../$1/II; cd ../
-#cp job-osepv52.pbs III/
-cd III; cp POSCAR INCAR KPOINTS job-osepv52.pbs ../../$1/III; cd ../
-#cp job-osepv52.pbs FM/
-cd FM; cp POSCAR INCAR KPOINTS job-osepv52.pbs ../../$1/FM; cd ../
