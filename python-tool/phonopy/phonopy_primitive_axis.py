2cb9895a2c2041a19abf84dcfde02060325f8ea0
yw-fang <fyuewen@gmail.com>
2018-07-26 15:30:59 +0900

    check primitive_axis for phonopy and phono3py, script finished

--- python-tool/phonopy/phonopy_primitive_axis.py
+++ python-tool/phonopy/phonopy_primitive_axis.py
@@ -11,7 +11,7 @@ __status__ = "Development"
 __creation_date__ = "July 26, 2018"
 
 """
-This script read two files, i.e. PPOSCAR and BPOSCAR,
+This script reads two files, i.e. PPOSCAR and BPOSCAR,
 the output is the primitive axis that can be
 used for phonopy or phono3py calculations.
 
@@ -26,6 +26,7 @@ au, bu, cu are read from the BPOSCAR, ap, bp, cp are read from PPOSCAR
 2018 July 26th
 """
 
+print("####################    Attention!!!      #######################")
 print("Before using this script, please run 'phonopy --symmetry --tol=0.01' ")
 print("BPOSCAR and PPOSCAR should exisit in current directory")
 
@@ -46,4 +47,6 @@ lattice_vector_uc = np.matrix(lattice_vector_uc)
 # print('lattice_vector_uc in nunpy matrix is', lattice_vector_uc)  # matrix
 
 primitive_axis = np.linalg.solve(lattice_vector_uc.T, lattice_vector_pc.T)
+print('\n')
+print('primitive_axis is \t')
 print(primitive_axis)
