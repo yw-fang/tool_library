c4160321d806f3ede1e96d0e0ebb11edfebdc6cf
yw-fang <fyuewen@gmail.com>
2018-08-02 21:31:15 +0900

    still failure;

---
+++ linuxshell-tool/aiida-completion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+eval (_VERDI_COMPLETE=source verdi)
+
+# Y.-W. FANG 2018 July 30th
--- linuxshell-tool/aiida-env.sh
+++
@@ -1,5 +0,0 @@
-#Y.-W. FANG 2018 July 30th
-source activate aiida  # activate the virtual environment
-eval "$(_VERDI_COMPLETE=source verdi)"  # completion command
-# For older versions of aiida_core, the completion command is
-# eval "$(verdi completioncommand)"
