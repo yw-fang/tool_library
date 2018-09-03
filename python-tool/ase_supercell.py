909665f22c5a85667760addb8b9d37b93fd5917e
yw-fang <fyuewen@gmail.com>
2018-09-03 23:57:40 +0900

    add author information, format the script

--- python-tool/ase_supercell.py
+++ python-tool/ase_supercell.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
 import ase.io.vasp as aiv
+
+__author__ = "Yue-Wen FANG"
+__maintainer__ = "Yue-Wen FANG"
+__email__ = "fyuewen@gmail.com"
+__status__ = "Development"
+__creation_date__ = "September 3, 2018"
+
 cell = aiv.read_vasp("POSCAR.vasp")
-aiv.write_vasp("POSCAR.222.ase", cell*(1,2,1), label='222supercell',
+aiv.write_vasp("POSCAR.222.ase", cell*(1, 2, 1), label='222supercell',
                direct=True,
                sort=True
                )
