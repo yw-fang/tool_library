413237ca9fd49a1b78a0d2c426eece04b8766c78
yw-fang <fyuewen@gmail.com>
2018-09-19 11:58:34 +0900

    fix error, redirect the output file to current dir

--- linuxshell-tool/mac/generate-folder-hierarchy-mac.sh
+++ linuxshell-tool/mac/generate-folder-hierarchy-mac.sh
@@ -1,1 +1,3 @@
-find ~/. -print | sed -e 's;[^/]*/;|--->;g;s;--->|; |;g' > ~/hierarchy.txt
+#This shell will generate the file and folder hierarchy for you in mac OS system
+#Author: Y.-W.FANG 2018 Setepmber 19th
+find ./ -print | sed -e 's;[^/]*/;|--->;g;s;--->|; |;g' > ./hierarchy.txt
