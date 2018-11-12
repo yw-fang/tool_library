30c4b02511cd93cbd204aa1635fa8fdcdc148612
yw-fang <fyuewen@gmail.com>
2018-11-12 23:59:24 +0900

    remove a line

--- python-tool/vn-poscar/vn-poscar-BiBaTiO3.py
+++ python-tool/vn-poscar/vn-poscar-BiBaTiO3.py
@@ -22,12 +22,12 @@ coordinates = p.structure[0:sum_natoms]
 fractional_coord_list = []
 for i in coordinates:
     fractional_coord_list.append(i.frac_coords)
-    
+
 
 df = pd.DataFrame({'frac_coordinates':fractional_coord_list,'number': range(1,sum_natoms+1)},index = range(1,sum_natoms+1))
 
 #split the 'frac_coordinates' column into three columns of 'x', 'y', and 'z'
-df[['x','y','z']] = pd.DataFrame(df.frac_coordinates.values.tolist(), index=df.index) 
+df[['x','y','z']] = pd.DataFrame(df.frac_coordinates.values.tolist(), index=df.index)
 
 df1 = df.copy()
 df2 = df1.sort_values(by=['y'], ascending=False)
@@ -35,6 +35,5 @@ number = df2.number
 
 # In this mode, every ABO3 layer contains 5 atoms
 # hence I group them by every 5 atoms
-for i,j,k,l,m in zip(*[iter(number)]*5): 
-#     print(i,j,k,l,m)
+for i,j,k,l,m in zip(*[iter(number)]*5):
     print("{},{},{},{},{}".format(str(i),str(j), str(k), str(l), str(m)))
