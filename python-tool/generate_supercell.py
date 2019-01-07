e3e1b0c668b6b2e271c05ffd2a8ebccc047a91f7
yw-fang <fyuewen@gmail.com>
2019-01-06 19:09:38 -0800

    start to consider the implementation of no sleective dynamics

--- python-tool/generate_supercell.py
+++ python-tool/generate_supercell.py
@@ -18,18 +18,22 @@ without this tag.
 
 p = Poscar.from_file('POSCAR')
 sd = p.selective_dynamics
-for x in sd:
-    for y in sd:
-        if y[0] == 'False' or 'F':
-            y[0] = 'True'
-            # print(y[0])
-        if y[1] == 'False' or 'F':
-            y[1] = 'True'
-        if y[2] == 'Flase' or 'F':
-            y[2] = 'True'
-c = p
-c.structure.make_supercell([2, 2, 3])
-c.natoms.append(1)
-
-c.structure.to(filename='S223POSCAR')
-# print(c.structure.to(fmt="poscar"))
+if sd:
+    print(sd)  # start to consider the implementation of no sleective dynamics
+else:
+    print('No selective dynamics')
+# for x in sd:
+#     for y in sd:
+#         if y[0] == 'False' or 'F':
+#             y[0] = 'True'
+#             # print(y[0])
+#         if y[1] == 'False' or 'F':
+#             y[1] = 'True'
+#         if y[2] == 'Flase' or 'F':
+#             y[2] = 'True'
+# c = p
+# c.structure.make_supercell([2, 2, 3])
+# c.natoms.append(1)
+# 
+# c.structure.to(filename='S223POSCAR')
+# # print(c.structure.to(fmt="poscar"))
