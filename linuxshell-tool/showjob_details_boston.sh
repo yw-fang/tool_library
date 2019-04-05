656e7479a5cee87d39d6774411ff2d0ec824e11c
yw-fang <fyuewen@gmail.com>
2019-04-05 17:13:58 +0900

    add usage

--- linuxshell-tool/showjob_details_boston.sh
+++ linuxshell-tool/showjob_details_boston.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# This is used to print the working directory of the job in Boston cluster
+# of Kyoto University
 qstat -j $1 | grep cwd | gawk '{print $2}'
+# Usage: showjob_details_boston.sh job-ID
