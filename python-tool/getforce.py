042855c97c47428a7f4b188b1316e4bf0621b590
yw-fang <fyuewen@gmail.com>
2019-04-16 18:45:24 +0900

    initial getforce, #2

---
+++ python-tool/getforce.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import ...
+
+__author__ = "Yue-Wen FANG"
+__maintainer__ = "Yue-Wen FANG"
+__email__ = "fyuewen@gmail.com"
+__status__ = "Development"
+__creation_date__ = "April 16th, 2019"
+__revision_date__ = "XX XXth, 2019"
+
+"""
+Get forces of atoms from OUTCAR file.
+To do
+"""
