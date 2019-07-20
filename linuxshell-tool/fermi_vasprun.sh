1e27b847945bf3c854d71273b6036c900ea3bd4d
yw-fang <fyuewen@gmail.com>
2019-07-20 13:09:05 +0900

    update fermi energy in vasprun.xml file, compatible to mac os

---
+++ linuxshell-tool/fermi_vasprun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#author: Y.-W. Fang contact: fyuewen@gmail.como
+#This shell script accepts two input parameters, the first one ($1) is Fermi level;
+#the second one ($2) is the operation system.
+EF=$1
+OS=$2
+
+if [ $OS == 'mac' ]
+then 
+	echo $OS
+	sed -i '' 's/.*efermi.*/\ \ \ \<i\ name=\"efermi\"\>\ \ \ \ \ \ '$EF' \<\/i\>/' vasprun.xml
+else
+	sed -i 's/.*efermi.*/\ \ \ \<i\ name=\"efermi\"\>\ \ \ \ \ \ '$EF' \<\/i\>/' vasprun.xml
+fi
+
+
+# sed -i '' 's/.*efermi.*/\ \ \ \<i\ name=\"efermi\"\>\ \ \ \ \ \ 3.672 \<\/i\>/' vasprun.xml
+
+####for mac#####
+#sed -i '' 's/.*efermi.*/\ \ \ \<i\ name=\"efermi\"\>\ \ \ \ \ \ '$EF' \<\/i\>/' vasprun.xml
+
+####for linux#####
+#sed -i 's/.*efermi.*/\ \ \ \<i\ name=\"efermi\"\>\ \ \ \ \ \ '$EF' \<\/i\>/' vasprun.xml
