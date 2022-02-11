c1a24fb6f97b3acb480c0c2b0a6c2fce6ab08a9e
yw-fang <fyuewen@gmail.com>
2022-02-11 21:22:26 +0800

    update add instructions

--- linuxshell-tool/targz.sh
+++ linuxshell-tool/targz.sh
@@ -1,3 +1,4 @@
 env GZIP=-9 tar -zcvf $1.tar.gz $1
-file $1.tar.gz
-echo "if the file format is tar.xz, first mv file.tar.gz to file.tar.xz, then use untarxz.sh to extract"
+# file $1.tar.gz
+echo "in some cases when extracting file, 'tar -xvf' does not work, in that case"
+echo "irst mv file.tar.gz to file.tar.xz, then use untarxz.sh to extract"
