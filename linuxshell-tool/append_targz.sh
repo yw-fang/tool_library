e9bd2f564fcbb9996d1f5484fb342690acb1a985
yw-fang <fyuewen@gmail.com>
2022-02-19 22:56:47 +0100

    append/add new files to an exsiting tar.gz file

---
+++ linuxshell-tool/append_targz.sh
@@ -0,0 +1,24 @@
+#author: Yuewen Fang
+#date: 2022 Feb 19
+#Purpose of this file: append new files to an exsiting tar.gz file
+echo "usage: append_tar name_of_tar.gz_file new_file1 new_file2 new_file3 .... "
+gzip -d $1
+echo $1
+tar_file=`echo $1 | sed 's/.gz//g'`
+echo $tar_file
+#tar uvf $tar_file tarxz.sh
+i=2
+for arg do
+	if [ $arg != $1 ];then  # exclude the original tar.gz file 
+		tar uvf $tar_file $arg
+		printf "$arg was appended to $1 \n"
+#	      printf '%s\n' "Arg $i: $arg"
+        fi
+  i=$((i + 1))
+done
+gzip -9 $tar_file
+echo "Compressed done"
+
+# uncompress the tar.gz : gzip -d backup.tar.gz
+# add/update a file on the tar file : tar uvf backup.tar db_bkp_today.sql
+# compress the tar (with maximum compression if you want) : gzip -9 backup.tar
