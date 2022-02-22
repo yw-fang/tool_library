6e5f4f88d55b011e89b37e4929dea99bf5e8250d
yw-fang <fyuewen@gmail.com>
2022-02-22 09:42:10 +0100

    add echo instructions

--- linuxshell-tool/untarxz.sh
+++ linuxshell-tool/untarxz.sh
@@ -1,4 +1,5 @@
 echo "Note that if file extension is tar.gz, you may need change it to tar.xz"
+echo "In some cases, if it doesn't work, use gunzip first, then change the extracted tar to tar.xz, then use unxz file.tar.xz to get file.tar, finally use tar -xvf file.tar"
 echo "Input file is "$1
 tarfile=$1
 #echo "${x%.*}" # becomes file.tar
