d116fc6f4348cc7b99759c33a8ae8a2d8598bcc2
yw-fang <fyuewen@gmail.com>
2022-03-31 15:20:26 +0200

    add comments

--- linuxshell-tool/greptoten-fang.sh
+++ linuxshell-tool/greptoten-fang.sh
@@ -1,6 +1,7 @@
 #author: Yuewen Fang created at 2017
 #modification date: 2022 March 31, add function to print the energy for pwscf and add more input parameters
-echo "usage: greptoten-fang.sh arg1 arg2, where arg1 is the code name and arg2 is the file name"
+echo "usage: greptoten-fang.sh arg1 arg2"
+echo "arguments meaning: arg1 code (vasp/pw); arg2: file"
 CODE=$1
 file=$2
 if [ $CODE == 'vasp' ]
