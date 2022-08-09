55f69bf14bf28fbe95c0e64351a4423ad0a0d35a
yw-fang <fyuewen@gmail.com>
2022-08-09 12:57:57 +0200

    collect pwout files and tar the archived direcotry

---
+++ python-tool/sscha/collect-pwout.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# [Author] Yue-Wen Fang [Email] fyuewen@gmail.com
+#          Description
+# [Usage] collect-pwout.sh
+# [Purpose] collect the scf_*.out files after running QE force calculations for supercells from SSCHA code
+#          History
+#  HISTORY
+#     2022/08/09 : Script creation
+# ------------------------------------------------------------------
+mkdir pwout
+pattern="scf_"
+for _dir in *"${pattern}"*; do
+    [ -d "${_dir}" ] && dir="${_dir}" && echo "${dir}"
+    cp "${dir}"/scf_*.out pwout/
+done
+tar -zcvf pwout.tar.gz pwout
