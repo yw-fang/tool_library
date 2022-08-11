60bf824bdcfd83eb1a0975f0b5d9684cb51524a9
yw-fang <fyuewen@gmail.com>
2022-08-11 23:20:31 +0200

    bugs exist

--- python-tool/calypso-tools/extract-enthalpy/extract_enthalpy_entropy-calypso.py
+++ python-tool/calypso-tools/extract-enthalpy/extract_enthalpy_entropy-calypso.py
@@ -30,9 +30,10 @@ col_names = ['preliminary_order', 'No', 'E_eV_atom', 'spg_symbol_tol_0.1', 'spg_
 df = pd.read_csv(crys_rslt_file, skiprows=[0], names=col_names, delim_whitespace=True, usecols=[0,1,2])
 
 # select the first 100 structures with lowest energy
-df_100 = df.sort_values(by=['E_eV_atom'])[:4]
+df_100 = df.sort_values(by=['E_eV_atom'])[:3]
 df_100['Spg_num_opt'] = np.nan
 df_100['Spg_sym_opt'] = np.nan
+df_100['Entropy_eV_cell'] = np.nan
 print(df.head(10))
 
 # read the CONTCAR in the folders 1 to 3
@@ -75,6 +76,30 @@ for dir_name in range(1,4):
     # how many atoms in the opt_struct
     n_atoms = opt_struct.num_sites
     # print(spg_symbol_opt)
+
+    if os.path.isfile(dir_name + '/' + "OSZICAR"):
+        with open(dir_name + '/' + "OSZICAR", 'r') as f:
+            for line in f:
+                if 'E0=' in line:
+                    E0 = float(line.split()[4])
+                    # print(E0)
+                    # preserve E0_per_atom having 8 decimal places
+                    E0_per_atom = round(E0/n_atoms, 8) # enthalpy eV normalized to per atom
+                    # save E0_per_atom to the column E_eV_atom in the df_100
+                    df_100.loc[df_100['preliminary_order'] == float(dir_name), 'E_eV_atom'] = E0_per_atom
+                    break
+    else:
+        # raise error that OSZICAR does not exist in the folder
+        raise FileNotFoundError('OSZICAR does not exist in the folder ' + dir_name)
+        # E0_per_atom = np.nan
+    # grep the EENTRO from OUTCAR and save the value to the floating variable entropy using shell
+    entropy = os.popen('grep "EENTRO" ' + dir_name + '/' + "OUTCAR").read().split()[-1]
+    entropy = float(entropy)  # eV per cell; convert into floating
+    print(float(entropy))
+    # alternatively, we can use in shell
+    # entropy = os.system('grep "EENTRO" '+dir_name+'/'+'OUTCAR'+' | tail -1 | awk \'{print $5}\'')
+    # however, here, python's round will make the very small entropy into 0.00000000, so donot use it here.
+    df_100.loc[df_100['preliminary_order'] == float(dir_name), 'Entropy_eV_cell'] = round(entropy, 8)
 
 # # select the first 100 structures with lowest energy
 # df_100 = df.sort_values(by=['E_eV_atom'])[:2]
