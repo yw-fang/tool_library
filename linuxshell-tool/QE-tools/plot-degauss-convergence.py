78df94e64cf61ccd68d134055a2c3315bc157d82
yw-fang <fyuewen@gmail.com>
2022-09-28 17:59:13 +0200

    fix yticks

--- linuxshell-tool/QE-tools/plot-degauss-convergence.py
+++ linuxshell-tool/QE-tools/plot-degauss-convergence.py
@@ -29,8 +29,9 @@ ax.set_ylabel('Energy (Ry)')
 plt.xlim(0, 0.1)
 # set the x and y ticks
 # plt.xticks([0, 0.02, 0.04, 0.06, 0.08, 0.1])
-plt.yticks([])
+# plt.yticks([])
 # set the x and y grid
 plt.grid(True, which='both')
+# set legend
 # save the figure
 fig.savefig('convergence.pdf', dpi=72)
