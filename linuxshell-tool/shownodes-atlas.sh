00fd43b472ea681ad9c6aaeaa16be2bbfefb9647
yw-fang <fyuewen@gmail.com>
2023-01-26 10:13:49 +0100

    correct

--- linuxshell-tool/shownodes-atlas.sh
+++ linuxshell-tool/shownodes-atlas.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 #this script was created by Yuewen Fang at CFM on Jan 26,2023. 
 #the purpose is to see the information (CPUs, memory) of each node.
-#  The CPUS(A/I/O/T) column in the output of the sinfo command shows the number of allocated, idle, other, and total CPUs on each node. 
+#  The CPUS(A/I/O/T) column in the output of the sinfo command shows the number of allocated, idle, other, and total CPUs on each node.
+
 partitions=("regular" "long" "xlong" "serial" "test" "large" "xlarge")
 
 #mthod1, show information overview
@@ -16,6 +16,6 @@ partitions=("regular" "long" "xlong" "serial" "test" "large" "xlarge")
 
 #method 2, show the node individually
  for partition in "${partitions[@]}"; do
-  sinfo -N -p regular --format=%N,%C,%D,%t,%P,%m| awk -F"," '{available = $2 - $3; print $1 " " $2 " " $3 " " $4 " " $5 " " available " " $6}'
-  echo "NODELIST CPUS(A/I/O/T) NODES STATE PARTITION 0 MEMORY"
+  sinfo -N -p $partition --format=%N,%C,%D,%t,%P,%m| awk -F"," '{available = $2 - $3; print $1 " " $2 " " $3 " " $4 " " $5 " " available " " $6}'
 done
+  echo "NODELIST CPUS(A/I/O/T) NODES STATE PARTITION 0 MEMORY"
